7f87f341c5b66389e861e2674c348f1a59a02a4e
Ashley Hong <ashley.hong@duke.edu>
2023-03-20 23:20:06 -0400

    remove implicit wire insertMultDivNop

--- processor-main/processor/processor.v
+++ processor-main/processor/processor.v
@@ -69,13 +69,13 @@ module processor(
 
     // Fetch stage
     wire [31:0] fetch_PC_out, PCAfterJump;
-	fetchControl fetch_stage(address_imem, fetch_PC_out, PCAfterJump, reset, ~clock, latchWrite || insertNopForMultDiv, ctrl_branch); // TODO: implement PCafterJump and jump ctrl
+	fetchControl fetch_stage(address_imem, fetch_PC_out, PCAfterJump, reset, ~clock, latchWrite, ctrl_branch); // TODO: implement PCafterJump and jump ctrl
 
     // FD Latch
     wire [31:0] FD_PCout, FD_InstOut, FD_branchCheck;
     // mux_2 checkFDflush(FD_branchCheck, ctrl_branch, q_imem, nop);
-    register32 FD_PCreg(FD_PCout, fetch_PC_out, ~clock, latchWrite || insertNopForMultDiv, reset);
-    register32 FD_InstReg(FD_InstOut, q_imem, ~clock, latchWrite || insertNopForMultDiv, reset);
+    register32 FD_PCreg(FD_PCout, fetch_PC_out, ~clock, latchWrite, reset);
+    register32 FD_InstReg(FD_InstOut, q_imem, ~clock, latchWrite, reset);
 
     // Decode stage
     decodeControl decode_stage(ctrl_readRegA, ctrl_readRegB, FD_InstOut);
